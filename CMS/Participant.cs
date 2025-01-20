7e6707430b6d6c1b56f92757537624cd6c9b221e
ManojramRagu <manojramragu@gmail.com>
2025-01-20 16:36:27 +0530

    Participant Class created

---
+++ CMS/Participant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS
+{
+    internal class Participant
+    {
+        public List<string> RegisteredSessions { get; set; }
+
+        public Participant()
+        {
+            RegisteredSessions = new List<string>();
+        }
+
+        public void RegisterSession(string sessionName)
+        {
+            RegisteredSessions.Add(sessionName);
+        }
+    }
+}
