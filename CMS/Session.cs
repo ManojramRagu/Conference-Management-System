50fa0b6ead6f055cd9a1d6e0af3b210b02255ebf
assassin871 <saijvikneshan@gmail.com>
2025-01-20 16:58:51 +0530

    session class added

---
+++ CMS/Session.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS
+{
+    internal class Session
+    {
+        // Attributes
+        public int SpeakerID { get; set; }
+        public string Name { get; set; }
+
+        // Methods
+        public void AssignToConference()
+        {
+            // Logic for assigning the session to a conference
+            Console.WriteLine("Session assigned to conference.");
+        }
+
+        public void EditDetails()
+        {
+            // Logic for editing session details
+            Console.WriteLine("Session details edited.");
+        }
+    }
+}
