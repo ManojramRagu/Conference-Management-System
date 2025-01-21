5f427161e3cc322eb8008ac06fd1d7cca3d3ce60
assassin871 <saijvikneshan@gmail.com>
2025-01-21 12:58:19 +0530

    registration ui update

--- CMS/Registration.Designer.cs
+++ CMS/Registration.Designer.cs
@@ -33,58 +33,78 @@
             this.loginBtn = new System.Windows.Forms.Button();
             this.Password = new System.Windows.Forms.Label();
             this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // usernameBox
             // 
-            this.usernameBox.Location = new System.Drawing.Point(322, 176);
+            this.usernameBox.Location = new System.Drawing.Point(350, 170);
             this.usernameBox.Name = "usernameBox";
-            this.usernameBox.Size = new System.Drawing.Size(100, 22);
+            this.usernameBox.Size = new System.Drawing.Size(134, 22);
             this.usernameBox.TabIndex = 0;
             this.usernameBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Username
             // 
             this.Username.AutoSize = true;
-            this.Username.Location = new System.Drawing.Point(245, 180);
+            this.Username.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(255, 170);
             this.Username.Name = "Username";
-            this.Username.Size = new System.Drawing.Size(73, 16);
+            this.Username.Size = new System.Drawing.Size(89, 19);
             this.Username.TabIndex = 1;
             this.Username.Text = "Username:";
             this.Username.Click += new System.EventHandler(this.label1_Click);
             // 
             // loginBtn
             // 
-            this.loginBtn.Location = new System.Drawing.Point(311, 263);
+            this.loginBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.loginBtn.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.Location = new System.Drawing.Point(284, 260);
             this.loginBtn.Name = "loginBtn";
-            this.loginBtn.Size = new System.Drawing.Size(122, 23);
+            this.loginBtn.Size = new System.Drawing.Size(169, 31);
             this.loginBtn.TabIndex = 2;
             this.loginBtn.Text = "Create Account";
-            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.UseVisualStyleBackColor = false;
             this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
             // 
             // Password
             // 
             this.Password.AutoSize = true;
-            this.Password.Location = new System.Drawing.Point(246, 213);
+            this.Password.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(255, 207);
             this.Password.Name = "Password";
-            this.Password.Size = new System.Drawing.Size(70, 16);
+            this.Password.Size = new System.Drawing.Size(87, 19);
             this.Password.TabIndex = 3;
             this.Password.Text = "Password:";
             // 
             // PasswordBox
             // 
-            this.PasswordBox.Location = new System.Drawing.Point(322, 213);
+            this.PasswordBox.Location = new System.Drawing.Point(350, 207);
             this.PasswordBox.Name = "PasswordBox";
             this.PasswordBox.PasswordChar = '*';
-            this.PasswordBox.Size = new System.Drawing.Size(100, 22);
+            this.PasswordBox.Size = new System.Drawing.Size(134, 22);
             this.PasswordBox.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(248, 107);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 31);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Registration Page";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Registration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.PasswordBox);
             this.Controls.Add(this.Password);
             this.Controls.Add(this.loginBtn);
@@ -104,6 +124,7 @@
         private System.Windows.Forms.Button loginBtn;
         private System.Windows.Forms.Label Password;
         private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
